12023094bfc748d7b9a8594b868dae11021a03da
David A McClain <david@uhhuhyeah.com>
2011-02-05 12:21:32 -0800

    updated spec to check against new data hash names

--- spec/model_un_spec.rb
+++ spec/model_un_spec.rb
@@ -3,7 +3,8 @@ require 'model_un'
 describe ModelUn do
   
   it "should be able to read the data hash" do
-    ModelUn::US_STATES.should_not == nil
+    ModelUn::US_STATE_NAMES.should_not == nil
+    ModelUn::US_STATE_ABBR.should_not == nil
   end
   
   it "should return an abbreviation for a full state" do
