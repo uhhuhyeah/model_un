6f1847a7b575e73cb2889011c37ee8492beaa006
David A McClain <david@uhhuhyeah.com>
2011-02-04 19:32:55 -0800

    Added rspec
    Signed-off-by: David A McClain <david@uhhuhyeah.com>

---
+++ spec/model_un_spec.rb
@@ -0,0 +1,13 @@
+require 'model_un'
+
+describe 'ModelUn' do
+  
+  it "should be able to read the data hash" do
+    US_STATES.should_not == nil
+  end
+  
+  it "should return an abbreviation for a full state" do
+    state = 'California'
+    ModelUn.convert(state).should == 'CA'
+  end
+end
