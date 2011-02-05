7cc1c21ed3b583367bd69d713cf789a437f5c229
David A McClain <david@uhhuhyeah.com>
2011-02-04 19:58:36 -0800

    Returns abbr for known us state!
    Signed-off-by: David A McClain <david@uhhuhyeah.com>

--- lib/model_un.rb
+++ lib/model_un.rb
@@ -1,5 +1,21 @@
 module ModelUn
   require "model_un/data"
   
-  
+  class << self
+    
+    def convert(string)
+      self.convert_state_name(string.upcase)
+    end
+    
+    def convert_state_abbr(state_abbr)
+      self.convert_state_abbreviation(state_abbr)
+    end
+    
+    def convert_state_abbreviation(state_abbr)
+    end
+    
+    def convert_state_name(state_name)
+      return US_STATE_ABBR[US_STATE_NAMES.index(state_name)]
+    end
+  end
 end
--- spec/model_un_spec.rb
+++ spec/model_un_spec.rb
@@ -3,7 +3,7 @@ require 'model_un'
 describe 'ModelUn' do
   
   it "should be able to read the data hash" do
-    US_STATES.should_not == nil
+    ModelUn::US_STATES.should_not == nil
   end
   
   it "should return an abbreviation for a full state" do
